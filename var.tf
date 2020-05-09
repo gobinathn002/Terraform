854b0b963cbc376002a1803ef781b7a230f91269
gobinathn002 <38713873+gobinathn002@users.noreply.github.com>
2020-05-09 13:48:23 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "privatekey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "privatekey.pub"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ec2-user"
+}
